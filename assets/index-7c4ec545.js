84d3f972b673f20c81b66b6318dcbf222f31c476
yeyunjianshi <yeyunjianshi@users.noreply.github.com>
2023-02-23 05:48:36 +0000

    deploy: 58cc579c5b2e927d547a76758653cd3fb292d12b

--- assets/index-7c4ec545.js
+++ assets/index-7c4ec545.js
@@ -1,7 +1,7 @@
 (function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&s(c)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const n="/AA/assets/typescript-f6ead1af.svg";function l(i){let r=0;const o=s=>{r=s,i.innerHTML=`count is ${r}`};i.addEventListener("click",()=>o(r+1)),o(0)}document.querySelector("#app").innerHTML=`
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/AA/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${n}" class="logo vanilla" alt="TypeScript logo" />
